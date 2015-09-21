03d36433f32be915182faf481c9d69ee773a1c30
Lucas Tsen <lucas.tsen@cameronandwilding.com>
2015-09-21 16:18:43 +0100

    removed goutte, edited CW feature file for D8, edited behat runner to handle variables

--- Behat/run-behat.sh
+++ Behat/run-behat.sh
@@ -1,21 +1,39 @@
 #!/usr/bin/env bash
 
-#  Start PhantomJS webdriver
-sh ../Servers/start_phantomjs_webdriver.sh
+## Assign script variables
+## $1 passed from Jenkins job / command line - the tags to be used.
+## $2 passed from Jenkins job / command line - the profile to be used.
+TAG=$1
+PROFILE=$2
 
-#  Copy project specific feature files into the submodule.
+## Copy files to behat folder for execution
 cp ../../Behat_Test_Files/Project.feature features/
+cp -R ../../Behat_Test_Files/images .
 
-#  Run Behat tests
-#  $1 passed from Jenkins job / command line - the tags to be used.
-#  $2 passed from Jenkins job / command line - the profile to be used.
-printf "\nStarting the Behat tests...\n"
+## Handle behat executing logic
+## Extra profiles should be added after the last if
 
-#  Jenkins / Command line
-bin/behat --tags=@$1 -p $2
+#  Selenium Server
+if [ $PROFILE = "firefox" ] || [ $PROFILE = "chrome"  ]
+then
+   sh ../Servers/start_selenium_server.sh;
+   bin/behat --tags=@$TAG -p $PROFILE
+fi
 
-#  Remove project specific feature files into the submodule.
+#  PhantomJS
+if [ $PROFILE = "phantomjs" ]
+then
+   sh ../Servers/start_phantomjs_webdriver.sh;
+   bin/behat --tags=@$TAG -p $PROFILE
+fi
+
+## Begin Teardown
+#  Remove copied files
 rm features/Project.feature
+rm -R images/
 
 #  Stop PhantomJS webdriver
 sh ../Servers/stop_phantomjs_webdriver.sh
+
+#  Stop Selenium server
+sh ../Servers/stop_selenium_server.sh
