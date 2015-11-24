573e635351ef0f642dc70ff46afa47dd01563f84
Brendan MacDonald <brendan.macdonald@cameronandwilding.com>
2015-11-24 13:03:13 +0000

    Updates to runner

--- Behat/run-behat.sh
+++ Behat/run-behat.sh
@@ -17,6 +17,9 @@ fi
 rsync ../../Behat_Test_Files/Project.feature features/
 rsync -av --exclude-from=../../Behat_Test_Files/images/.gitkeep ../../Behat_Test_Files/images/ images
 
+## Move previous result files to a History folder
+mv ../Results/Behat/*.html ../Results/Behat/History
+
 ## Handle behat executing logic
 ## Extra profiles should be added after the last if
 ## Add profile to teardown where necessary
